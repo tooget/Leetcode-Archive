9d94c26b3e79638ff5b389606a34403159c15160
tooget <tooget@gmail.com>
2019-08-08 18:25:38 +0900

    update 596.classes-more-than-5-students.sql

---
+++ 596.classes-more-than-5-students.sql
@@ -0,0 +1,53 @@
+--
+-- @lc app=leetcode id=596 lang=mysql
+--
+-- [596] Classes More Than 5 Students
+--
+-- https://leetcode.com/problems/classes-more-than-5-students/description/
+--
+-- database
+-- Easy (36.13%)
+-- Total Accepted:    50.4K
+-- Total Submissions: 139.4K
+-- Testcase Example:  '{"headers": {"courses": ["student", "class"]}, "rows": {"courses": [["A", "Math"], ["B", "English"], ["C", "Math"], ["D", "Biology"], ["E", "Math"], ["F", "Computer"], ["G", "Math"], ["H", "Math"], ["I", "Math"]]}}'
+--
+-- There is a table courses with columns: student and class
+-- 
+-- Please list out all classes which have more than or equal to 5 students.
+-- 
+-- For example, the table:
+-- 
+-- 
+-- +---------+------------+
+-- | student | class      |
+-- +---------+------------+
+-- | A       | Math       |
+-- | B       | English    |
+-- | C       | Math       |
+-- | D       | Biology    |
+-- | E       | Math       |
+-- | F       | Computer   |
+-- | G       | Math       |
+-- | H       | Math       |
+-- | I       | Math       |
+-- +---------+------------+
+-- 
+-- 
+-- Should output:
+-- 
+-- 
+-- +---------+
+-- | class   |
+-- +---------+
+-- | Math    |
+-- +---------+
+-- 
+-- 
+-- 
+-- 
+-- Note:
+-- The students should not be counted duplicate in each course.
+-- 
+--
+# Write your MySQL query statement below
+SELECT class FROM courses GROUP BY class HAVING COUNT(distinct student) >= 5
