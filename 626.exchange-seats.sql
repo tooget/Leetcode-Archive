fb2bdeb3cd79b4a7a139bac9e144d6e92cfe3f5e
tooget <tooget@gmail.com>
2019-08-16 16:24:11 +0900

    update 626.exchange-seats.sql

---
+++ 626.exchange-seats.sql
@@ -0,0 +1,71 @@
+--
+-- @lc app=leetcode id=626 lang=mysql
+--
+-- [626] Exchange Seats
+--
+-- https://leetcode.com/problems/exchange-seats/description/
+--
+-- database
+-- Medium (55.42%)
+-- Total Accepted:    28.4K
+-- Total Submissions: 51.3K
+-- Testcase Example:  '{"headers": {"seat": ["id","student"]}, "rows": {"seat": [[1,"Abbot"],[2,"Doris"],[3,"Emerson"],[4,"Green"],[5,"Jeames"]]}}'
+--
+-- Mary is a teacher in a middle school and she has a table seat storing
+-- students' names and their corresponding seat ids.
+-- The column id is continuous increment.
+-- 
+-- 
+-- Mary wants to change seats for the adjacent students.
+-- 
+-- 
+-- Can you write a SQL query to output the result for Mary?
+-- 
+-- 
+-- 
+-- 
+-- +---------+---------+
+-- |    id   | student |
+-- +---------+---------+
+-- |    1    | Abbot   |
+-- |    2    | Doris   |
+-- |    3    | Emerson |
+-- |    4    | Green   |
+-- |    5    | Jeames  |
+-- +---------+---------+
+-- 
+-- For the sample input, the output is:
+-- 
+-- 
+-- 
+-- 
+-- +---------+---------+
+-- |    id   | student |
+-- +---------+---------+
+-- |    1    | Doris   |
+-- |    2    | Abbot   |
+-- |    3    | Green   |
+-- |    4    | Emerson |
+-- |    5    | Jeames  |
+-- +---------+---------+
+-- 
+-- 
+-- Note:
+-- If the number of students is odd, there is no need to change the last one's
+-- seat.
+-- 
+--
+# Write your MySQL query statement below
+
+-- SET @TotalRecordCnt = (SELECT COUNT(id) FROM seat);
+
+SELECT
+    CASE WHEN id % 2 = 0 THEN id - 1
+        --  ELSE (CASE WHEN id = @TotalRecordCnt THEN id
+         ELSE (CASE WHEN id = (SELECT COUNT(id) FROM seat) THEN id
+                     ELSE id + 1
+               END) 
+    END AS 'id',
+    student
+FROM seat
+ORDER BY id;
